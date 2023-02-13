d8987d9f1564e88cf16f8eedbee2ea4723c70707
KaiX9 <koh_kaixiang_8@hotmail.com>
2023-02-13 11:36:38 +0800

    number guess program with server app completed

--- src/main/java/sg/edu/nus/iss/app/ServerApp.java
+++ src/main/java/sg/edu/nus/iss/app/ServerApp.java
@@ -11,7 +11,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
 
-public class App 
+public class ServerApp 
 {
     public static void main( String[] args ) throws IOException {
         
